54394f1d9f9ce92cdd26c568f9ad7421c4ecf00d
Patrick Mwangi <pwmwangi2@gmail.com>
2024-06-03 17:06:29 +0300

    fix build errors

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -63,7 +63,7 @@ export async function scrapeAmazonProduct(url: string) {
         title,
         currentPrice: Number(currentPrice) || Number(originalPrice),
         originalPrice: Number(originalPrice) || Number(currentPrice),
-        priceHistory: [],
+        priceHistory: String,
         discountRate: Number(discountRate),
         category: "category",
         reviewsCount: 100,
